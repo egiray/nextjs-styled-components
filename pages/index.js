4861105f07f5414a97b5a4bd4cad774b459e2445
egiray <egiray@gmx.net>
2019-08-16 22:52:33 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size:20px;
+  color:blue;
+`
+
+const Home = () => (
+  <Title>Welcome to Next.js!</Title>
+)
+
+
+export default Home
